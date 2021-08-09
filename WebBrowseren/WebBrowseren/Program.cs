a6062ece70f5764eb6d7668f08599ffd0cb3bb49
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-09 13:07:37 +0200

    comments

--- WebBrowseren/WebBrowseren/Program.cs
+++ WebBrowseren/WebBrowseren/Program.cs
@@ -11,13 +11,19 @@ namespace WebBrowseren
     {
         static async Task Main(string[] args)
         {
+            // create a http client
             HttpClient client = new HttpClient();
+            // requests a website
             HttpResponseMessage response = await client.GetAsync("https://www.zbc.dk");
 
+            // makes sure the status code is success
             response.EnsureSuccessStatusCode();
+            // html content
             string body = await response.Content.ReadAsStringAsync();
 
+            // regex to remove html tags
             Regex regex = new Regex("(<[^^]*\\shtml>)|(<*html*>)");
+            // prints html content without html tags
             Console.WriteLine(regex.Replace(body, "<<<>>>"));
 
             Console.ReadLine();
