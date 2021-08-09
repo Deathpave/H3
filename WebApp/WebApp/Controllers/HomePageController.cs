b46dc5d42f5de6427632b0026d729be2bbd25c42
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-09 11:47:39 +0200

    Rest API ?

---
+++ WebApp/WebApp/Controllers/HomePageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomePageController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<TesterClass> Home()
+        {
+            return Enumerable.Range(0, 2).Select(index => new TesterClass { Alpha = "Kage er godt", Xone = index }).ToArray();
+        }
+    }
+}
---
+++ WebApp/WebApp/TesterClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class TesterClass
+    {
+        public string Alpha { get; set; }
+
+        public int Xone { get; set; }
+    }
+}
