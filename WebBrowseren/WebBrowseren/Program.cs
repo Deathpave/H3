cf97759fc893cb7a3572baa442b4e0ce4c03dba7
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-09 13:04:38 +0200

    Console webbrowser

---
+++ WebBrowseren/WebBrowseren/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace WebBrowseren
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://www.zbc.dk");
+
+            response.EnsureSuccessStatusCode();
+            string body = await response.Content.ReadAsStringAsync();
+
+            Regex regex = new Regex("(<[^^]*\\shtml>)|(<*html*>)");
+            Console.WriteLine(regex.Replace(body, "<<<>>>"));
+
+            Console.ReadLine();
+        }
+    }
+}
