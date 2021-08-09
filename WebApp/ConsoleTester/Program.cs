b336719eb3c934ed615d4dacca8e1194899816a8
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-09 11:50:27 +0200

    vs

---
+++ WebApp/ConsoleTester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using static System.Net.WebRequestMethods;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest.Create()
+        }
+    }
+}
