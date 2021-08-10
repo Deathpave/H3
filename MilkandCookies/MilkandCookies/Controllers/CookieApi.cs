05ddbbe7f81f4cb8ce7d6ef16d80141080c8ef7a
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-10 12:32:39 +0200

    MilkandCookies

---
+++ MilkandCookies/MilkandCookies/Controllers/CookieApi.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilkandCookies.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CookieApi : Controller
+    {
+        [HttpGet]
+        public string GetCookie()
+        {
+            return "cookie";
+        }
+    }
+}
