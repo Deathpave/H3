cef0da11fed20ade0adfd8e0986d808074f1cec4
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-12 10:21:46 +0200

    start of project

--- Vittighedsmaskinen/Vittighedsmaskinen/obj/Debug/net5.0/Vittighedsmaskinen.AssemblyInfo.cs
+++ Vittighedsmaskinen/Vittighedsmaskinen/obj/Debug/net5.0/Vittighedsmaskinen.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("57e9ee11-2c9f-4d61-ae4e-d29b300a7534")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vittighedsmaskinen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
