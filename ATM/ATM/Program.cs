3638c79f284b41ca07cf96cbc6494cb23104155d
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-08-17 13:58:05 +0200

    creation of account and card implemented

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -13,9 +13,23 @@ namespace ATM
         private static ATMMachine atm = new ATMMachine();
         private static AccountManager accountManager = new AccountManager();
         private static CardManager cardManager = new CardManager();
+        private static CreditCard card;
+        private static BankAccount account;
         static void Main(string[] args)
         {
-            Console.WriteLine("Please ");
+            Console.WriteLine("Fethcing you new bank account");
+            account = accountManager.NewBankAccount();
+            Console.WriteLine("\nEnter name to get a new creditcard");
+
+            card = cardManager.NewCreditCard(Console.ReadLine(), account.AccountNumber);
+            Console.WriteLine("This is your pincode, dont forget it!\n" + card.Password);
+            Console.WriteLine("Press enter to continue");
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.ReadLine();
+
         }
     }
 }
