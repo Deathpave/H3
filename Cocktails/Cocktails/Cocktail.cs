f6a7b7fa3a14ed88bc51f4fc2188a47ff0318007
Deathpave <49186768+Deathpave@users.noreply.github.com>
2021-08-23 13:57:08 +0200

    first attempt

---
+++ Cocktails/Cocktails/Cocktail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocktails
+{
+    public class Cocktail
+    {
+        public string Name { get; private set; }
+        public List<Ingredient> Ingredients { get; private set; }
+
+        public Cocktail(string name, List<Ingredient> ingredients)
+        {
+            Name = name;
+            Ingredients = ingredients;
+        }
+    }
+}
---
+++ Cocktails/Cocktails/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocktails
+{
+    public class Ingredient
+    {
+        public string Name { get; private set; }
+        public string Amount { get; private set; }
+        public Ingredient(string name, string amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+    }
+}
